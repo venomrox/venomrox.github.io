af0dfda92109699a47818a0929781541a16fd7b2
kenreilly <kenneth.reilly@hotmail.com>
2016-04-24 10:19:21 -0500

    Scrolling text / blinking cursor

--- ts/venom.js
+++ ts/venom.js
@@ -1,2 +1,51 @@
-document.querySelector("header").classList.add("fade-in");
+var Venom;
+(function (Venom) {
+    var TerminalWriter = (function () {
+        function TerminalWriter(element) {
+            this.element = element;
+        }
+        TerminalWriter.prototype.init = function () {
+            //debugger;
+            this.text = this.element.innerHTML;
+            this.element.innerHTML = "";
+            this.cursor = document.createElement("b");
+            this.cursor.innerHTML = "_";
+            this.element.appendChild(this.cursor.cloneNode(true));
+            for (var i = 0, ii = this.text.length; i != ii; ++i) {
+                var timeout = (i + 1) * TerminalWriter.refreshRate;
+                window.setTimeout(this.render.bind(this, i, ii), timeout);
+            }
+        };
+        TerminalWriter.prototype.render = function (i, ii) {
+            //debugger;
+            this.element.innerHTML = this.text.slice(0, i + 1);
+            this.element.appendChild(this.cursor.cloneNode(true));
+        };
+        TerminalWriter.InitAll = function () {
+            //debugger; 
+            var elements = document.querySelectorAll(TerminalWriter.selector);
+            for (var i = 0, ii = elements.length; i != ii; ++i) {
+                var writer = new TerminalWriter(elements[i]);
+                TerminalWriter.instances.push(writer);
+                writer.init();
+            }
+        };
+        TerminalWriter.refreshRate = 40;
+        TerminalWriter.selector = ".terminal-writer";
+        TerminalWriter.instances = [];
+        return TerminalWriter;
+    }());
+    Venom.TerminalWriter = TerminalWriter;
+    var MainPage = (function () {
+        function MainPage() {
+        }
+        MainPage.Init = function () {
+            document.querySelector("header").classList.add("fade-in");
+            TerminalWriter.InitAll();
+        };
+        return MainPage;
+    }());
+    Venom.MainPage = MainPage;
+})(Venom || (Venom = {}));
+Venom.MainPage.Init();
 //# sourceMappingURL=venom.js.map
--- ts/venom.ts
+++ ts/venom.ts
@@ -1,1 +1,70 @@
-﻿document.querySelector("header").classList.add("fade-in");
+﻿namespace Venom {
+
+    export class TerminalWriter {
+
+        private static refreshRate: number = 40;
+
+        private static selector: string = ".terminal-writer";
+
+        private static instances: Array<TerminalWriter> = [];
+
+        private element: HTMLElement;
+
+        private text: string;
+
+        private cursor: HTMLElement;
+
+        constructor(element: HTMLElement) { this.element = element; }
+
+        protected init() {
+
+            //debugger;
+            
+            this.text = this.element.innerHTML;
+            this.element.innerHTML = "";
+
+            this.cursor = <HTMLElement>document.createElement("b");
+            this.cursor.innerHTML = "_";
+            this.element.appendChild(this.cursor.cloneNode(true));
+
+            for (var i = 0, ii = this.text.length; i != ii; ++i) {
+
+                var timeout = (i + 1) * TerminalWriter.refreshRate;
+                window.setTimeout(this.render.bind(this, i, ii), timeout);
+            }
+        }
+
+        protected render(i, ii) {
+
+            //debugger;
+
+            this.element.innerHTML = this.text.slice(0, i + 1);
+            this.element.appendChild(this.cursor.cloneNode(true));
+        }
+
+        public static InitAll() {
+
+            //debugger; 
+
+            var elements = document.querySelectorAll(TerminalWriter.selector);
+            for (var i = 0, ii = elements.length; i != ii; ++i) {
+
+                var writer = new TerminalWriter(<HTMLElement>elements[i]);
+                TerminalWriter.instances.push(writer);
+
+                writer.init();
+            }
+        }
+    }
+
+    export class MainPage {
+
+        public static Init(): void {
+
+            document.querySelector("header").classList.add("fade-in");
+            TerminalWriter.InitAll();
+        }
+    }
+}
+
+Venom.MainPage.Init();
